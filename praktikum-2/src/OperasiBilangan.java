fdccb7ab7aa8ad578088b030ef4077a2ea2c6e8b
Muhammad Farhan <farname01@gmail.com>
2026-09-14 23:13:27 +0700

    Latihan 5

--- praktikum-2/src/OperasiBilangan.java
+++ praktikum-2/src/OperasiBilangan.java
@@ -7,7 +7,7 @@ public class OperasiBilangan {
         System.out.print("Masukkan Bilangan Pertama: ");
         int bil1 = sc.nextInt();
 
-        System.out.print("Masukkan Bilangan Kedua: ");
+        System.out.print("Masukkan Bilangan kedua: ");
         int bil2 = sc.nextInt();
 
         // Operasi Aritmatika
@@ -15,9 +15,9 @@ public class OperasiBilangan {
         System.out.println("Selisih: " + (bil1 - bil2));
         System.out.println("Kali: " + (bil1 * bil2));
         System.out.println("Bagi: " + (bil1 / bil2));
-        System.out.println("Sisa Bagi: " + (bil1 % bil2));
+        System.out.println("Selisih Bagi: " + (bil1 % bil2));
 
-        // Operasi Relasional
+        // Operasi Relational
         System.out.println("Lebih Besar (>): " + (bil1 > bil2));
         System.out.println("Lebih Kecil (<): " + (bil1 < bil2));
         System.out.println("Sama Dengan (==): " + (bil1 == bil2));
