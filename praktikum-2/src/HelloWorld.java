359359a62e43645536ae6056e682e5f9176576c5
Muhammad Farhan <farname01@gmail.com>
2026-09-14 21:55:43 +0700

    Struktur Dasar Program Java

--- praktikum-2/src/HelloWorld.java
+++ praktikum-2/src/HelloWorld.java
@@ -1,6 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Halo, Farhan!");
+
+        System.out.print("Nama: ");
+        System.out.print("Budi");
+        System.out.println();
+        System.out.println("Ini Baris Baru");
     }
 }
 
