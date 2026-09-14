27077c7333724d8a8aa65800d09c6aa06c8f0a36
Muhammad Farhan <farname01@gmail.com>
2026-09-14 23:06:15 +0700

    Latihan 4

--- praktikum-2/src/KonversiSuhu.java
+++ praktikum-2/src/KonversiSuhu.java
@@ -4,11 +4,11 @@ public class KonversiSuhu {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("masukkan Suhu Celcius: ");
+        System.out.print("Masukkan Suhu Celcius: ");
         double Celcius = sc.nextDouble();
 
         double Fahrenheit = Celcius * 9/5 + 32;
 
-        System.out.println(Celcius + " Celcius = " + Fahrenheit  + " Fahrenheit");
+        System.out.println(Celcius + " Celcius = " + Fahrenheit + " Fahrenheit");
     }
 }
