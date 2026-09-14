f6da774c3a86628100b2f2f4546700a39b08d36f
Muhammad Farhan <farname01@gmail.com>
2026-09-14 22:28:00 +0700

    Latihan 1

--- praktikum-2/src/BiodataSaya.java
+++ praktikum-2/src/BiodataSaya.java
@@ -1,9 +1,11 @@
 public class BiodataSaya {
     public static void main(String[] args) {
-        // Program menampilkan biodata sederhana mahasiswa
-        System.out.print("Muhammad Farhan"); // Mencetak nama tanpa pindah baris
-        System.out.print(" - "); // Pemisah antara nama dan NIM
-        System.out.println("2025573010080"); // Mencetak NIM lalu pindah baris
-        System.out.println("Program Studi: Teknik Informatika"); // Mencetak program studi
+        /* Program menampilkan biodata mahasiswa sederhana */
+        // Menampilkan Nama mahasiswa
+        System.out.println("Nama: Muhammad Farhan");
+        // Menampilkan NIM mahasiswa
+        System.out.println("NIM: 2025573010080");
+        // Menampilkan Program Studi mahasiswa
+        System.out.println("Program Studi: Teknik Informatika");
     }
 }
