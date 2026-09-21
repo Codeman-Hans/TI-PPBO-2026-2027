cfc28aaca4d42e2a40678d1cfc2a16b167d34490
Muhammad Farhan <farname01@gmail.com>
2026-09-22 00:30:55 +0700

    Struktur Kontrol Keputusan (If-Else dan Switch-Case)

--- praktikum-3/src/Latihan2.java
+++ praktikum-3/src/Latihan2.java
@@ -4,6 +4,7 @@ public class Latihan2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        // Memasukkan bilangan bulat
         System.out.print("Masukkan bilangan pertama: ");
         int a = sc.nextInt();
 
@@ -13,6 +14,7 @@ public class Latihan2 {
         System.out.print("Masukkan bilangan ketiga: ");
         int c = sc.nextInt();
 
+        // Mengecek kondisi yang mana bilangan terbesar
         int terbesar;
         if (a >= b && a >= c) {
             terbesar = a;
